a19dbb340cb7dbdaf4cc342d118606b770b014bb
sweety1lime <sweety.lime@bk.ru>
2022-03-25 15:47:05 +0300

    Сделан генератор по кнопке: обложка и заголовки(заголовки можно редактировать)

---
+++ Lime_Editor/wwwroot/js/DemoGenerator.js
@@ -0,0 +1,19 @@
+﻿function genTitle(){
+
+var newDiv = document.createElement('div');
+    newDiv.innerHTML = '<h1 contenteditable="tru" class="text-center">Hello world!!!</h1>';
+    var script = document.getElementsByClassName("test")[0];
+    var parent = script.parentNode;
+    parent.insertBefore(newDiv, script);
+    document.getElementById("collapsed").hidden = true;
+}
+
+function genCover() {
+
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = '<article class="cover"><h4 class="cover__uptitle">your company</h4><h1 class="cover__title">Title/TagLine</h1><p class="cover__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis odio vel varius sollicitudin.</p></article>';
+    var script = document.getElementsByClassName("test")[0];
+    var parent = script.parentNode;
+    parent.insertBefore(newDiv, script);
+    document.getElementById("collapsed").hidden = true;
+}
