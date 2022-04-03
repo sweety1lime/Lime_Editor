2fa29eb3af92cda3856f84b6ec568d8fbb844704
Даниил Серяк <Danilka228_322@outlook.com>
2022-04-03 14:52:18 +0300

    Сделана выгрузка html файла, перенос css и images в новую папку

--- Lime_Editor/Controllers/HomeController.cs
+++ Lime_Editor/Controllers/HomeController.cs
@@ -47,9 +47,63 @@ namespace Lime_Editor.Controllers
         [HttpPost]
         public IActionResult EditTemplatesPost(string html)
         {
-            System.IO.File.WriteAllText("test.txt", html);
 
-            Debug.WriteLine(html);
+            var currentHtml = "<!DOCTYPE html> \n " +
+                "<html lang=\"ru_RU\"> " +
+                "\n <head> \n " +
+                "<meta charset=\"utf - 8\"> \n" +
+                "<meta name=\"viewport\" content=\"width = device - width, initial - scale = 1.0\"> \n" +
+                "<title>Тестовый</title> \n" +
+                "<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\"/> \n" +
+                "<link href=\"css/bootstrap.css\" rel=\"stylesheet\"/> \n" +
+                "<link href=\"css/mainMeow.css\" rel=\"stylesheet\" \n/>" +
+                "<link href=\"css/responsive.css\" rel=\"stylesheet\" \n/>" +
+                "<link href=\"css/Themes.css\" rel=\"stylesheet\" \n/>" +
+                "</head> \n" +
+                "<body> \n" +
+                html+
+                "\n </body> \n" +
+                "</html>";
+            currentHtml = currentHtml.Replace("/images", "images");
+            currentHtml = currentHtml.Replace("contenteditable=\"true\"", "contenteditable=\"false\"");
+            string directory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Тестовые проекты";
+            if (!System.IO.Directory.Exists(directory))
+                System.IO.Directory.CreateDirectory(directory);
+            System.IO.File.WriteAllText(directory + @"\test.html", currentHtml);
+            Directory.CreateDirectory(directory + "\\css");
+            Directory.CreateDirectory(directory + "\\images");
+            string[] reqiredCss = new[] { "bootstrap.min.css", "bootstrap.css", "mainMeow.css", "responsive.css" , "Themes.css" };
+            string[] filePaths = Directory.GetFiles(_environment.WebRootPath + "\\css\\main\\"); //файл2.css, файл3.css, файл4.css
+            foreach (var filePath in filePaths)
+            {
+                if (reqiredCss.Contains(filePath.Substring(filePath.LastIndexOf("\\") + 1)))
+                {
+                    string path = filePath;
+                    string newPath = directory+ "\\css\\" + filePath.Substring(filePath.LastIndexOf("\\") + 1);
+                    FileInfo fileInf = new FileInfo(path);
+                    if (fileInf.Exists)
+                    {
+                        fileInf.CopyTo(newPath, true);
+                    }
+                }
+
+            }
+
+            string[] pathImg = Directory.GetFiles(_environment.WebRootPath + "\\images\\"); //файл2.css, файл3.css, файл4.css
+            foreach (var filePath in pathImg)
+            {
+                if (html.Contains(filePath.Substring(filePath.LastIndexOf("\\") + 1)))
+                {
+                    string path = filePath;
+                    string newPath = directory + "\\images\\" + filePath.Substring(filePath.LastIndexOf("\\") + 1);
+                    FileInfo fileInf = new FileInfo(path);
+                    if (fileInf.Exists)
+                    {
+                        fileInf.CopyTo(newPath, true);
+                    }
+                }
+
+            }
 
             return Ok();
         }
