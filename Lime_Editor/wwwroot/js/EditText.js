fa0c9e00a89c6e0fe096d6b8f7d72f499c7ba4f2
sweety1lime <sweety.lime@bk.ru>
2022-05-06 22:00:25 +0300

    Добавлен новый редактор текста(баг при генерации)

--- Lime_Editor/wwwroot/js/EditText.js
+++ Lime_Editor/wwwroot/js/EditText.js
@@ -15,27 +15,67 @@ function formatDoc(sCmd, sValue) {
 function validateMode() {
     if (!document.compForm.switchBox.checked) { return true; }
     alert("Uncheck \"Показать HTML\"."); /* убрать галочку из "Показать HTML" */
+   
     oDoc.focus();
     return false;
 }
 
-function setDocMode(bToSource) {
+function setDocMode() {
 
     var editor = document.getElementById("userSpace");
-    var textBox = null;
+    var editorContinue = document.getElementById("userSpaceContinue");
+    var textBox = document.getElementById("textBox");
     var html = "";
 
     if (editor == null) {
         alert("Not found userSpace");
         return;
     }
-    else {
 
-        textBox = document.getElementById("textBox");
-        html = textBox.innerHTML;
-        editor.innerHTML = html;
+    if (editorContinue == null) {
+        alert("Not found userSpaceContinue");
+        return;
     }
 
+    if (textBox == null) {
+        alert("Not found textBox");
+        return;
+    }
+
+    html = textBox.innerHTML;
+    html += editorContinue.innerHTML;
+    //alert(html)
+    editor.innerHTML = html;
+    editorContinue.innerHTML = "";
+    document.getElementById("boom").hidden = true;
+
+    //alert(html)
+    //html = textBox.innerHTML;
+    //editor.innerHTML = html;
+    /*
+        var oContent;
+        if (bToSource) {
+            oContent = document.createTextNode(oDoc.innerHTML);
+            oDoc.innerHTML = "";
+            var oPre = document.createElement("pre");
+            oDoc.contentEditable = false;
+            oPre.id = "sourceText";
+            oPre.contentEditable = true;
+            oPre.appendChild(oContent);
+            oDoc.appendChild(oPre);
+            document.execCommand("defaultParagraphSeparator", false, "div");
+        } else {
+            if (document.all) {
+                oDoc.innerHTML = oDoc.innerText;
+            } else {
+                oContent = document.createRange();
+                oContent.selectNodeContents(oDoc.firstChild);
+                oDoc.innerHTML = oContent.toString();
+            }
+            oDoc.contentEditable = true;
+        }
+        oDoc.focus();
+        */
 }
 
 function printDoc() {
---
+++ Lime_Editor/wwwroot/js/NewTextEdit.js
@@ -0,0 +1,32 @@
+﻿// Elements
+const elements = document.querySelectorAll('.btn');
+
+// Event
+elements.forEach(element => {
+	element.addEventListener('click', () => {
+		let command = element.dataset['element'];
+		
+		if (command == 'createLink' || command == 'insertImage') {
+			let url = prompt('Enter the link here:', 'http://');
+			document.execCommand(command, false, url);
+		} else {
+			document.execCommand(command, false, null);
+		}
+	});
+});
+
+var options = {
+    placeholder: 'Waiting for your precious content',
+    theme: 'snow'
+};
+
+var editor = new Quill('#editor', options);
+function closeEditor() {
+
+    var justHtmlContent = document.getElementById('justHtml');
+    var justHtml = editor.root.innerHTML;
+    justHtmlContent.innerHTML = justHtml;
+    document.getElementById("editor").hidden = true;
+    document.getElementById("collapsedbox").hidden = true;
+    document.getElementById("collapsedbtn").hidden = true;
+}
